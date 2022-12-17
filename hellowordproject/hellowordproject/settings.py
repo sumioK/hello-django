1c50b790a6a709611a5bf874d3aab8a4706c3cd8
sumiok <tap.miwo@gmail.com>
2022-12-17 16:03:44 +0900

    urls.py

--- hellowordproject/hellowordproject/settings.py
+++ hellowordproject/hellowordproject/settings.py
@@ -90,6 +90,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
+# passwdのバリデーションを設定
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',

eb30ec9adf7aa5201e7d634f4beadce5fc587f3a
sumiok <tap.miwo@gmail.com>
2022-12-17 16:33:07 +0900

    BASE DIR

--- hellowordproject/hellowordproject/views.py
+++ hellowordproject/hellowordproject/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 def helloworldfunction(request):
   returnedobject = HttpResponse('<h1>hello world</h1>')
   return returnedobject
+
+class HelloWorldClass(TemplateView):
+  template_name = 'hello.html'

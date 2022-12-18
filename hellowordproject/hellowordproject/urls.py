d6763c0df6e921a9b25708f2ba5dce8c73604dea
sumiok <tap.miwo@gmail.com>
2022-12-18 13:56:42 +0900

    urls app

--- hellowordproject/hellowordproject/urls.py
+++ hellowordproject/hellowordproject/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import helloworldfunction, HelloWorldClass
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('helloworld/', helloworldfunction),
     path('helloworld2/', HelloWorldClass.as_view())
+    path('', include('helloworldapp.urls'))
 ]
